8aa86baa67fbcd3c6a4d1f5f377dfeca8907355f
Julien Drolet-Noel <eurojulien@gmail.com>
2014-02-01 22:05:48 -0500

    Timer fonctionnel

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		long startTime = System.nanoTime();
+		
 		Sudoku.getSudokuFromFile(args[0]);
 		
 		Sudoku.printDebugSudoku();
@@ -19,6 +21,10 @@ public class Main {
 		Sudoku.printDebugSudoku();
 		
 		System.out.println("Sudoku final est valide : " + Sudoku.EstValide());
+	
+		long endTime = System.nanoTime();
+		
+		System.out.println("Elapsed Time : " + (endTime - startTime)/(1000000) + " milliseconds");
 	}
 
 }
