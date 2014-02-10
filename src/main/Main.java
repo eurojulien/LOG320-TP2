68814321fcc49051d40d79e30d1d1872671d58ce
Julien Drolet-Noel <eurojulien@gmail.com>
2014-02-10 16:18:00 -0500

    old master

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,31 +8,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-        Sudoku.getSudokuFromFile(args[0]);
+		long startTime = System.nanoTime();
+		
+		Sudoku.getSudokuFromFile(args[0]);
 		
 		Sudoku.printDebugSudoku();
-
-        if(Sudoku.EstValide()){
-            System.out.println("Sudoku de base est valide");
-
-            long startTime = System.nanoTime();
-            long endTime = 0;
-
-            SudokuSolver ss = new SudokuSolver(Sudoku.getSudoku());
-
-            if(ss.backTracking()){
-                System.out.println("Sudoku résolut!");
-                ss.printDebugSudoku();
-                endTime = System.nanoTime();
-            }else{
-                System.out.println("Sudoku non résolut!");
-            }
-            System.out.println("Elapsed Time : " + (endTime - startTime)/(1000000) + " milliseconds");
-
-        }else{
-            System.out.println("Sudoku de base n'est pas valide");
-        }
-
+		
+		System.out.println("Sudoku de base est valide : " + Sudoku.EstValide());
+		
+		Sudoku.backTracking(0, 0);
+		
+		Sudoku.printDebugSudoku();
+		
+		System.out.println("Sudoku final est valide : " + Sudoku.EstValide());
+	
+		long endTime = System.nanoTime();
+		
+		System.out.println("Elapsed Time : " + (endTime - startTime)/(1000000) + " milliseconds");
 	}
 
 }
--- src/main/Sudoku.java
+++ src/main/Sudoku.java
@@ -1,8 +1,6 @@
 package main;
 
 import java.math.*;
-import java.util.ArrayList;
-
 public class Sudoku {
 
 	
@@ -10,8 +8,8 @@ public class Sudoku {
 	 * Description Sudoku
 	 * 
 	 * 0 = Valeur de la case
-	 * # = Nombre de donnï¿½es > 0 de la colonne/ligne
-	 * # sera utilisï¿½ plus tard pour un algorithme plus avance
+	 * # = Nombre de données > 0 de la colonne/ligne
+	 * # sera utilisé plus tard pour un algorithme plus avance
 	 * _ _ _ _ _ _ _ _ _ _ _ _ _
 	 * | 0 0 0 | 0 0 0 | 0 0 0 | #
 	 * | 0 0 0 | 0 0 0 | 0 0 0 | #
@@ -28,119 +26,161 @@ public class Sudoku {
 	 *   # # #   # # #   # # # 
 	 */
 	private static final int TAILLE_MAX		= 9;
-	public static int[][] sudoku 			= new int[TAILLE_MAX + 1][TAILLE_MAX + 1];
-    private static Boolean[][][] tableauPossibilite;
-    public static ArrayList<int[]> listeOrdrePreferable;
-
+	private static int[][] sudoku 			= new int[TAILLE_MAX + 1][TAILLE_MAX + 1];
+	
+	
 	private Sudoku(){}
-
-    public static void setTableauPossibilite(Boolean[][][] tabPoss){
-        tableauPossibilite = tabPoss;
-    }
-
-	// Creation du sudoku a partir d'un fichier texte
+	
+	// Creation du sudoku a partir d'un fichier texte 
 	public static void getSudokuFromFile(String fileName){
-
+	
 		BinaryStdIn.setInputFile(fileName);
 		int[] columnCounter 	= new int[TAILLE_MAX];
 		int cell				= 0;
-
+		
 		for (int i = 0; i <= TAILLE_MAX; i ++){
-
+			
 			if (i < TAILLE_MAX){
-
+				
 				for(int j = 0; j <= TAILLE_MAX; j++){
-
-
+					
+					
 					// Lecture des cellules du sudoku
 					if(j < TAILLE_MAX){
-
+						
 						// Important, car '/n' et '/r' retournent '-1'
 						do{
 							cell 						= Character.getNumericValue((int) BinaryStdIn.readChar());
 						}while(cell < 0);
-
-						sudoku[i][j]				= cell;
-
-						// Calcul du nombre de donnï¿½es ou != 0 par range/colonne
+						
+						sudoku[j][i]				= cell;
+					
+						// Calcul du nombre de données ou != 0 par range/colonne
 						if(cell != 0){
-							sudoku[TAILLE_MAX][j]	++;
-							sudoku[i][TAILLE_MAX]	++;
+							sudoku[TAILLE_MAX][i]	++;
+							sudoku[j][TAILLE_MAX]	++;
 						}
-					}
+					}				
 				}
 			}
 		}
-
+		
 		BinaryStdIn.close();
 	}
-
-    public static int[][] getSudoku(){
-        return sudoku;
-    }
-
+	
 	// i : Position en X
 	// j : Position en Y
 	// k : Valeur numerique a verifier
 	public static boolean EstValide(int i, int j, int k){
-
+		
 		if(i > TAILLE_MAX || j > TAILLE_MAX) return false;
 		if(k == 0) return true;
-
+		
 		int offsetI = (int)Math.floor(i/3) * 3;
 		int offsetJ = (int)Math.floor(j/3) * 3;
-
+		
 		// Verification de la rangee
 		for(int z = 0; z < TAILLE_MAX; z ++){
-
+			
 			// Verification de la rangee
 			if(z != i){
 				if (sudoku[z][j] == k) return false;
 			}
-
+			
 			// Verification de la colonne
 			if(z != j){
 				if (sudoku[i][z] == k) return false;
 			}
-
+			
 			// Verification dans le carre 3x3
 			if((offsetI + z%3) != i && (offsetJ + (int)Math.floor(z/3)) != j){
-
+				
 				//System.out.println("i : " + i + " Offset I : " + offsetI + " Cell I " + z%3 + " j : " + j + " Offset J : " + offsetJ + " Cell J " + (int)Math.floor(z/3));
 				if(sudoku[offsetI + z%3][offsetJ + (int)Math.floor(z/3)] == k) return false;
 			}
 		}
-
+		
 		return true;
 	}
-
+	
 	// Verifie tout le sudoku
 	public static boolean EstValide(){
-
+		
 		for(int i = 0; i < TAILLE_MAX; i ++){
 			for(int j = 0; j < TAILLE_MAX; j ++){
 				if (!EstValide(i,j,sudoku[i][j])) return false;
 			}
 		}
-
+		
 		return true;
 	}
-
+	
 	// Fonction Debug
 	public static void printDebugSudoku(){
-
-		for (int i = 0; i < TAILLE_MAX; i ++){
-
+		
+		for (int i = 0; i < TAILLE_MAX + 1; i ++){
+			
 			System.out.println("- - - - - - - - -");
-
-			for (int j = 0; j < TAILLE_MAX; j ++){
-
-				System.out.print(sudoku[i][j] + " ");
+			
+			for (int j = 0; j < TAILLE_MAX + 1; j ++){
+				
+				System.out.print(sudoku[j][i] + " ");
 			}
-
+			
 			System.out.println("");
 		}
-
-        System.out.println(" = = = = = = = = = ");
+	}
+	
+	// Solver le sudoku
+	public static boolean backTracking(int i, int j){
+		
+		int nextI = i, nextJ = j;
+		boolean isSolved = false;
+		
+		// Recherche de la prochaine case a verifier (Valeur initiale doit etre = zero)
+		do{
+			if(nextJ < (TAILLE_MAX - 1)){
+				nextJ 	++;
+			}
+			else if(nextI < (TAILLE_MAX - 1)){
+				nextI 	++;
+				nextJ = 0;
+			}
+			else{
+				// Derniere case du tableau 2D Sudoku atteinte
+				// i = 8, j = 8
+				// doit faire le retour final !
+				
+				//System.out.println("LAST I : " + nextI + " LAST J : " + nextJ);
+				
+				isSolved = true;
+			}
+			
+		// Boucle pour sauter par dessus les cases ou il y a deja un chiffre
+		}while(sudoku[nextI][nextJ] != 0 && nextI != TAILLE_MAX-1 && nextJ != TAILLE_MAX-1);
+		
+		//System.out.println("Next I : " + nextI + " Next J : " + nextJ);
+		
+		// Verification des differentes possibilites
+		for(int cell = 1; cell <= TAILLE_MAX; cell ++){
+				
+				//System.out.println(i + ":" + j + " TRY : " + cell);
+				
+				if(EstValide(i, j, cell)){
+					sudoku[i][j] = cell;
+					if(isSolved){
+						return true;
+					}
+					
+					else if (backTracking(nextI, nextJ)) return true;
+				}
+		}
+	
+		//System.out.println("FALSE");
+		
+		// Re Initialisation de la case ...
+		sudoku[i][j] = 0;
+		
+		return isSolved;
 	}
 }
